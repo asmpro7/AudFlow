c388fe93bc294aa40419c30fb7fc7f82beade22f
Ahmed ElSaeed <ahmedmassad@std.mans.edu.eg>
2023-06-25 22:14:29 +0300

    make execute key #4

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ from flowlauncher import FlowLauncher
 from gtts import gTTS
 import os
 from playsound import playsound
-
-
+from pathlib import Path
+import json
 class AudFlow(FlowLauncher):
     def query(self, query):
+        base_dir = Path(__file__).resolve().parent
+        Flow_launcher_main_dir = base_dir.parent.parent
+        flow_settings_dir = Flow_launcher_main_dir / "Settings" / "Plugins" / "AudFlow" / "Settings.json"
+        if os.path.exists(flow_settings_dir):
+            with open(flow_settings_dir,"r") as f:
+                settings=json.load(f)
+        key=settings["execute_key"]
         output=[]
         try:
             if len(query.strip()) == 0:
@@ -27,7 +34,7 @@ class AudFlow(FlowLauncher):
                  if len(query) > 3 and ":" in query[0]:                
                       lang=query[1:3]
                       query=query[3:]
-                      if query[-1]=="|":
+                      if query.endswith(key):
                         output.clear()
 
                         myobj = gTTS(text=query, lang=lang, slow=False)
@@ -54,7 +61,7 @@ class AudFlow(FlowLauncher):
 
                     lang="en"
                       
-                    if query[-1]=="|":
+                    if query.endswith(key):
                                     output.clear()
 
                                     myobj = gTTS(text=query, lang=lang, slow=False)
@@ -71,7 +78,7 @@ class AudFlow(FlowLauncher):
                                     "IcoPath": "Images/stop.png"})
                                     
                     
-                    elif query[-1] !="|":
+                    elif not query.endswith(key):
                         output.append({
                         "Title": "Typing...",
                         "IcoPath": "Images/type.png"})
